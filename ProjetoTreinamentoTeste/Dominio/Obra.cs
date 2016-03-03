dd628611659781142bc0dfb0c95a150a2144d588
QueirozDavi <daviqueiroz92@gmail.com>
2016-03-03 16:37:01 -0300

    Criação do Domínio completo
    Davi e João

--- ProjetoTreinamentoTeste/Dominio/Obra.cs
+++ ProjetoTreinamentoTeste/Dominio/Obra.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Classe mãe
 
 namespace Dominio
 {
--- ProjetoTreinamentoTeste/Dominio/Pessoa.cs
+++ ProjetoTreinamentoTeste/Dominio/Pessoa.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Classe Mãe
+
 namespace Dominio
 {
     public class Pessoa
